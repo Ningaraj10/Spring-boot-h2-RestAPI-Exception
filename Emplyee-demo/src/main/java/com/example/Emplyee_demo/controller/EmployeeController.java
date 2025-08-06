6d6df65aa233ef3d5b79e400ee6b6bcb44f0830e
Ningaraj10 <ningaraj.pyati1994@gmail.com>
2025-08-06 22:56:07 +0530

    Commit

--- Emplyee-demo/src/main/java/com/example/Emplyee_demo/controller/EmployeeController.java
+++ Emplyee-demo/src/main/java/com/example/Emplyee_demo/controller/EmployeeController.java
@@ -1,6 +1,45 @@
 package com.example.Emplyee_demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.Emplyee_demo.model.Employee;
+import com.example.Emplyee_demo.service.EmployeeService;
+
 public class EmployeeController {
 
-	int a;
+	@Autowired
+	private EmployeeService service;
+	
+	  @GetMapping
+	    public List<Employee> getAll() {
+	        return service.getAll();
+	    }
+
+	    @GetMapping("/{id}")
+	    public Employee getById(@PathVariable Long id) {
+	        return service.getById(id);
+	    }
+
+	    @PostMapping
+	    public Employee create(@RequestBody Employee employee) {
+	        return service.create(employee);
+	    }
+
+	    @PutMapping("/{id}")
+	    public Employee update(@PathVariable Long id,  @RequestBody Employee employee) {
+	        return service.update(id, employee);
+	    }
+
+	    @DeleteMapping("/{id}")
+	    public void delete(@PathVariable Long id) {
+	        service.delete(id);
+	    }
 }
